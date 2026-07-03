657ee0f8cdc93cf5097a4cd23c02852517df631e
shukrulloh <shukrullohrajabov1@gmail.com>
2026-07-03 22:05:11 +0500

    fix:

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dmc_first_repo/',
+  base: './',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
